cfde4a18b76842bca2165f800fae31ef2720b9eb
Naveed latif <naveedlatif526@gmail.com>
2026-04-11 15:49:42 -0700

    update samesite

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -38,7 +38,7 @@ export const register = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
-            sameSite: "None",
+             sameSite: "Lax",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
         return res.json({
@@ -80,7 +80,7 @@ export const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
-            sameSite: "None",
+             sameSite: "Lax",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -151,8 +151,8 @@ export const logout = async (req, res) => {
     try {
         res.clearCookie('token', {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+             sameSite: "Lax",
+            secure: true,
         });
 
         return res.json({
